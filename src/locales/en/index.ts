c1f32efd9a6337a7e615df97575d2d75b2331836
Bastien Youssfi <bastien.youssfi@gmail.com>
2025-11-19 14:37:32 +0100

    feat: dashboard

--- src/locales/en/index.ts
+++
@@ -1,7 +0,0 @@
-import common from './common.json';
-import landing from './landing.json';
-
-export default {
-  common,
-  landing,
-};
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { devtools } from '@tanstack/devtools-vite'
+import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
-// https://vite.dev/config/
+import { fileURLToPath, URL } from 'node:url'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [devtools(), viteReact(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
